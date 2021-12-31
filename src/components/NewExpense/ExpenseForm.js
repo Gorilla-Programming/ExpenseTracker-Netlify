9671dd4990d9feb5537cd541261dae9863fe457c
Ankit Chaudhary <ankit.khalilabad@gmail.com>
2021-12-31 12:13:26 +0530

    Expense-Tracker-Second-Commit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -38,15 +38,15 @@ function ExpenseForm(props) {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" value={enteredTitle} onChange={titleHandler}/>
+          <input type="text" value={enteredTitle} onChange={titleHandler} required="True"/>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateHandler}/>
+          <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateHandler} required="True"/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountHandler}/>
+          <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountHandler} required="True"/>
         </div>
       </div>
       <div className="new-expense__actions">
